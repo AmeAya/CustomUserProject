9276590a07fca2f6a19946b86a05c03d307acaf6
ameaya <deobackstep@gmail.com>
2023-07-26 20:56:45 +0600

    Registration with Raw HTML and Python

--- user_app/views.py
+++ user_app/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from .forms import CustomUserCreationForm
 from django.shortcuts import redirect
+from .models import CustomUser
 
 
 def registrationView(request):
     if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        if password1 != password2:
+            return False
+        user = CustomUser(email=email)
+        user.set_password(password1)
+        user.save()
+        return redirect('')
+
     else:
         context = {'form': CustomUserCreationForm()}
         return render(request, 'registration_page.html', context)
