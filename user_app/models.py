59238a29fe691f5201b4dd6af1f2310e5906d52b
ameaya <deobackstep@gmail.com>
2023-07-26 20:06:19 +0600

    Add Forms and Custom admin

--- user_app/models.py
+++ user_app/models.py
@@ -1,14 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils import timezone
 from .managers import CustomUserManager
 
 
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)  # unique=True потому что email вместо username
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now())
 
     USERNAME_FIELD = 'email'  # Указываем поле которое необходимо для авторизации
